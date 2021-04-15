a6a08891510cfd1f1d37fb9242590bd95e65b7be
Ankit Hans <ankithans1947@gmail.com>
2021-04-15 15:45:38 +0530

    feat: search for products WIP

--- actions/actions.py
+++ actions/actions.py
@@ -6,22 +6,36 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
+import datetime as dt 
+from typing import Any, Text, Dict, List
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_show_time"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text=f"{dt.datetime.now()}")
+
+        return []
+
+
+class ActionSearch(Action):
+    def name(self) -> Text:
+        return "action_search_submit"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        product_name = tracker.get_slot("product_name")
+        dispatcher.utter_message(text=f"Good bro {product_name}")
+
+        return []
